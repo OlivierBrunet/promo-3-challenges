c623d42d4fa6b5944871ec276fe0994eac737f4d
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-09 19:07:37 +0200

    blackjack is fun

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -10,6 +10,8 @@ def play_game
     puts state_of_the_game(player_score, bank)
   end
 
+  puts build_message_for(game_outcome(bank, player_score))
+
 end
 
 def state_of_the_game(score, bank)
@@ -27,4 +29,14 @@ end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+  if outcome[1] > 21
+    puts "You are over 21... you loose."
+  elsif outcome[1] == 21
+    puts "Black Jack!"
+  elsif outcome[1] > outcome[0]
+    puts "You beat the bank! You win."
+  else
+    puts "Bank beats you! You loose."
+end
+
 end
