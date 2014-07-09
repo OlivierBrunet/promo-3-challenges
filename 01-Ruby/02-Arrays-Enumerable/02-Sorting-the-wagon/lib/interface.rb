4a4e953474d049a76642cf87c6957a5c44f2930d
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-09 15:40:09 +0200

    sorted

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -8,7 +8,16 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
-
+students << name if name != ""
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+students = wagon_sort(students)
+count = students.length
+last_student = students.last
+students.delete_at(-1)
+
+puts "Congratulations! Your Wagon has #{count} students:"
+
+print students.join(", ") + " and #{last_student}"
+
