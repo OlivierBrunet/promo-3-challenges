7bbf9a00767cec61627c2cca7e1158ee69b34f0b
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-09 11:53:22 +0200

    loops & recursion pas finie

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,15 +1,36 @@
 def sum_with_while(min,max)
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a while..end structure
+  sum = 0
+  count = min
+
+  while count <= max
+    sum += count
+    count += 1
+  end
+
+  return sum
 end
+
 
 def sum_with_for(min,max)
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a for..end structure
+
+  count = min
+  sum = 0
+
+  for count in (min..max)
+    sum += count
+  end
+  return sum
 end
+
 
 def sum_recursive(min,max)
   #TODO (optional): return the sum of the integers between min and max
   #CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
+  return min if min == max
+  min + sum_recursive(min + 1, max)
 end
 
