a45d13c46e9ce21ab4dbd1f76690ff99392856b6
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-09 20:53:57 +0200

    black jack solved

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,17 +2,18 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
-  bank = bank_score
-  player_score = 0
+  score_bank = bank_score
+  score_player = 0
 
-  while asking_for_card?(player_score)
-    player_score += pick_card
-    puts state_of_the_game(player_score, bank)
+  while asking_for_card?(score_player) == true
+    score_player += pick_card
+    puts state_of_the_game(score_player, score_bank)
   end
 
-  puts build_message_for(game_outcome(bank, player_score))
-
+  total = game_outcome(score_bank, score_player)
+  puts build_message_for(total)
 end
+
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
@@ -21,22 +22,24 @@ end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
-  puts "Card ? (type 'Y' or 'yes' for a new card)"
-  player_answer = gets.chomp
-
-  player_answer == "Y" || player_answer == "yes"
+  if score < 21
+    puts "Card ? (type 'Y' or 'yes' for a new card)"
+    answer = gets.chomp
+    answer == 'Y' || answer == 'yes'
+  else
+    false
+  end
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
   if outcome[1] > 21
-    puts "You are over 21... you loose."
+    "You are over 21... you loose."
   elsif outcome[1] == 21
-    puts "Black Jack!"
-  elsif outcome[1] > outcome[0]
-    puts "You beat the bank! You win."
+    "Black Jack!"
+  elsif outcome[1] < outcome[0]
+    "Bank beats you! You loose."
   else
-    puts "Bank beats you! You loose."
-end
-
+    "You beat the bank! You win."
+  end
 end
