0e4cb838446cf2633a17b62c4c45620d50972843
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-09 18:29:59 +0200

    Revert "Result before livecode"
    This reverts commit dbd0e91ceefe9c99705a152a5218c04d653682cc.

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,16 +1,11 @@
 def bank_score
   # TODO: Use Random to get a new random score
-  (16..21).to_a.sample
 end
 
 def pick_card
   # TODO: Use Random to get a new random card
-  (1..11).to_a.sample
 end
 
 def game_outcome(bank, score)
   # TODO: Take the bank and the score and output an array containing the bank and then the score
-  bank = bank_score
-  score = pick_card
-  game = [bank_score, pick_card]
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,37 +2,16 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
-  puts "Card ? (type 'Y' or 'yes' for a new card)"
-  user_decision = gets
-  while user_decision != ("Y" || "yes")
-    puts "Card ? (type 'Y' or 'yes' for a new card)"
-    user_decision = gets
-  end
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
-  score = pick_card
-  bank = bank_score
-  puts "Your score is #{score}, bank is #{bank}!"
 end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
-  state_of_the_game
-  if score <= 21
-    puts "Card ? (type 'Y' or 'yes' for a new card)"
-    answer = gets.chomp
-      while answer = ("Y" || "yes")
-        score += pick_card =
-      end
-  else
-    print "You lose"
-  end
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
-  if
-
 end
