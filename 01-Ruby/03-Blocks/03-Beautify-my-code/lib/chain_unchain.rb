fe8b1b2ccb1a0111a135ed37aa61c50f360840ee
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-10 15:25:55 +0200

    sans les nombres premiers

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -5,7 +5,8 @@ end
 
 def quote_prime_numbers(n)
   #TODO: refactor this method
-
   (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
 end
+
+quote_prime_numbers(100)
 
