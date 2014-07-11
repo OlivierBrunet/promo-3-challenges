cf072fed446032c4bb729c58f17c6ae5dceb30e8
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-11 12:25:44 +0200

    solved

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -23,4 +23,26 @@ end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+  somme = 0
+
+  list = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170,
+    "Happy Meal" => 580,
+    "Best Of Big Mac" => 590,
+    "Best Of Royal Cheese" => 430,
+  }
+
+  orders.each do |food|
+  somme += list[food]
+  end
+somme
 end
+
+calories_counter("Cheese Burger", "Happy Meal", "Sprite")
