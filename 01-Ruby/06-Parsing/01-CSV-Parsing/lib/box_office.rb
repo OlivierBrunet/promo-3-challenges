2f6a3b50e33a8f5ea40e6dcd3d19ede8d28d29b9
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-14 15:07:47 +0200

    fail

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -3,4 +3,9 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = '/lib/movies.csv'
+
+CSV.foreach(filepath, csv_options) do |row|
+  puts "#{row['file_name']}, a #{row['max_year']} movie - (id #{row['number']})"
 end
