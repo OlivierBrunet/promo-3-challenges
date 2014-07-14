3a87da57128df8df953b41dfdc2121286801957d
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-14 15:07:10 +0200

    fail

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,10 @@
 def array_to_hash(array)
   #TODO: implement the method :)
+  my_hash = Hash.new
+  array.each_with_index do |value, index|
+  my_hash[index] = value
+  end
+  my_hash
 end
+
+puts array_to_hash(["a","b","c"])
