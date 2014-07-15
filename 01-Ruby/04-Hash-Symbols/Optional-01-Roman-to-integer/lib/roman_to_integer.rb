88a35de67c8affd93ed195550b67bb3f35d155ed
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-15 10:30:55 +0200

    not solved

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,5 +1,20 @@
+roman = {
+I => 1,
+II => 2,
+III => 3,
+IV => 4,
+V => 5,
+X => 10,
+L => 50,
+C => 100,
+D => 500
+}
+
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+  roman.each do |key, value|
+  if roman_string == key then puts value
+  end
 end
 
 # testing your program
