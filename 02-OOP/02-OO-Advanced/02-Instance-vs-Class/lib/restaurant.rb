4911e2822055b951b82215d28e44495b4b1e3479
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-17 12:23:39 +0200

    ratings

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,27 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
+  attr_accessor :average_rating, :name, :address, :city
+
   def initialize(city, name)
-    #TODO: implement constructor with relevant instance variables
+    #TODO: implement constructor with rele]vant instance variables
+    @name = name
+    @address = address
+    @city = city
+    @average_rating = 0
+    @rating_array = []
   end
-  
+
   #TODO: implement #filter_by_city and #rate methods
-  
+  def rate(user_rate)
+    @rating_array << user_rate.to_f
+    @average_rating = @rating_array.inject(:+) / @rating_array.size
+  end
+
+  #def self.filter_by_city(restaurants, city)
+
+  #end
+
 end
+
+jules_verne = Restaurant.new("Paris", "Jules Verne")
