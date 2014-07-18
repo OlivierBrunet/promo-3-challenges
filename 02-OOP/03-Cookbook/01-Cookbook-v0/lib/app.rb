891e97592195cd761b69e458a1cb13263d9f74de
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-18 16:12:01 +0200

    solveconflict

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/app.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/app.rb
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 require_relative 'cookbook'    # You need to create this file!
 require_relative 'controller'  # You need to create this file!
 require_relative 'router'
@@ -9,4 +7,3 @@ cookbook = Cookbook.new(CSV_FILE)
 controller = Controller.new(cookbook)
 
 Router.new(controller).run
->>>>>>> 464991c50ed48ab5052705050b4077d6ee9ed6d5
