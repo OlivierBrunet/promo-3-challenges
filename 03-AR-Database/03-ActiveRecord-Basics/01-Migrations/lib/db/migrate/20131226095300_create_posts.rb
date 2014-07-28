20fdab85b083e3282986f3d4e1048fe75530592a
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-07-28 12:18:26 +0200

    solved

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |post|
+      post.string :name
+      post.string :source_url
+      post.datetime :created_at
+    end
   end
 end
